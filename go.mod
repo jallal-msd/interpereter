79dde0e2fdbfde41b03b0206b775b01bc0f9a3ae
jallal-msd <jallalblack@gmail.com>
2023-06-01 16:27:10 +0200

    br

---
+++ go.mod
@@ -0,0 +1,3 @@
+module itpr
+
+go 1.19
---
+++ token/token.go
@@ -0,0 +1,42 @@
+package token
+
+type TokenType string
+
+type Token struct {
+    Type TokenType 
+    Literal string
+}
+
+var keyword = map[string]TokenType {
+    "fn": FUNCTION,
+    "let":LET,
+}
+func LookupIdent(ident string) TokenType{
+    if tok, ok := keyword[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
+
+const (
+
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    IDENT ="IDENT"
+    INT  = "INT"
+
+    ASSIGN = "="
+    PLUS = "+"
+
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+)
